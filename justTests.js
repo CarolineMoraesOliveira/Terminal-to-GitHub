06e78c3ba71962fc61466c172ba2663657de8b77
Caroline M Oliveira <carolinemo.dev@gmail.com>
2023-07-26 16:49:20 -0300

    made a random answer to life questions

--- justTests.js
+++ justTests.js
@@ -1,14 +1,33 @@
 console.log ('Running main.js');
 
-function d6() {
-    var roll = Math.random(min, max);
-    var min = 1;
-    var max = 6;
-    return roll;
+var lifesAnswers = [
+    "It is certain.",
+    "It is decidedly so.",
+    "Without a doubt.",
+    "Yes – definitely.",
+    "You may rely on it.",
+    "As I see it, yes.",
+    "Most likely.",
+    "Outlook good.",
+    "Yes.",
+    "Signs point to yes.",
+    "Reply hazy, try again.",
+    "Ask again later.",
+    "Better not tell you now.",
+    "Cannot predict now.",
+    "Concentrate and ask again.",
+    "Don't count on it.",
+    "My reply is no.",
+    "My sources say no.",
+    "Outlook not so good.",
+    "Very doubtful."
+];
+
+function answer(){
+    var randomQuestion = lifesAnswers[Math.floor(Math.random() * lifesAnswers.length)];
+    return (randomQuestion);
 }
+var x = answer(
     
-var playerRoll = d6();
-console.log("The player rolled: " + playerRoll);
-
-
-
+);
+console.log(x);
